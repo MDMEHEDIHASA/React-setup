1ead9c024de2c3df44cd35803dd3b50152a5adcd
Mehedi <mmehedihasan514@gmail.com>
2021-02-28 01:17:30 +0600

    all the frontend added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-const App = ()=>{
-    return(
-        <div>
-            <h1>Welcome to Proshop</h1>
-        </div>
-    )
-}
+import React from "react";
+import {Container} from 'react-bootstrap';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from './screen/HomeScreen';
+import ProductScreen from './screen/ProductScreen';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+        <Route path="/" exact component={HomeScreen}/>
+        <Route path="/product/:id" component={ProductScreen}/>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+};
 
 export default App;
